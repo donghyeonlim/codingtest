e43716d1c50134447bbdda6a511cdad54108bd86
Limdonghyeon <chobo12768@gmail.com>
2022-01-14 22:23:09 +0900

    Create 문자열 내 p와 y의 개수.py

---
+++ 문자열 내 p와 y의 개수.py
@@ -0,0 +1,9 @@
+def solution(s):
+    countp = 0
+    county = 0
+    for i in s:
+        if i == 'p' or i == 'P':
+            countp += 1
+        elif i == 'y' or i == 'Y':
+            county +=1
+    return countp == county
