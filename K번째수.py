0644165f9d2e036f602d9fcf0d3d6978f59f895b
Donghyeon Lim <ldhyeon0891@naver.com>
2022-02-09 16:48:13 +0900

    Update K번째수.py

--- K번째수.py
+++ K번째수.py
@@ -1,8 +1,11 @@
 def solution(array, commands):
     answer = []
     for i in range(len(commands)):
-        a,b,c = commands
+        a = commands[i][0]
+        b = commands[i][1]
+        c = commands[i][2]
         list = array[a-1:b]
         list.sort()
         answer.append(list[c-1])
+    print(answer)
     return answer
